a0f2cbb57a6d506b92af35210d1ba553dfe8b9d6
Yasir <yzyasir@yahoo.com>
2021-05-11 09:08:24 -0400

    Working on the backend, still need to create the routes

--- server/models/adoption.model.js
+++ server/models/adoption.model.js
@@ -8,7 +8,14 @@ const adoptionSchema = new mongoose.Schema({
         required:[true, "Add a name for your pet"],
         minlength:[3, "Name needs to be 3 characters long"],
         maxlength:[12, "Pet name is too long, be reasonable please"]
-    }
+    },
+    description:{
+        type: String,
+        required:[true, "Add a description for your pet"],
+        minlength:[5, "Please add a longer description"], 
+        maxlength:[50, "Please make the description slightly smaller"]
+    },
+
 }, {timestamps:true}) //Here we are making time stamps for any of the data we make
 
 const Adoption = mongoose.model("Adoption", adoptionSchema); // Here we are registering our schema
