28489ff2119210d14ea4cc999c933356c6176876
Yasir <yzyasir@yahoo.com>
2021-05-20 23:13:38 -0400

    Got all routes working, made sure i am getting my api calls through axios console log. Just need to display the information next

--- client/src/views/AdoptionListPage.jsx
+++ client/src/views/AdoptionListPage.jsx
@@ -1,1 +1,26 @@
 import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+// import {Link} from '@reach/router';
+
+
+const AdoptionListPage = (props) => {
+
+    const [adoptee, setAdoptee] = useState();
+
+    useEffect(() =>{ 
+        axios.get("http://localhost:8000/api/pets")
+        // .then(res => console.log(res.data)) // Checked in console to see whether or not its present
+        // .then(res => console.log(res.data.pet)) // Went into pets to see the information we get in the form of json
+        .then(res => setAdoptee(res.data.pet)) // Now we are putting our information into set state
+    })
+
+    return(
+        <div>
+            <h3>These pets are looking for a good home</h3>
+        </div>
+    )
+
+
+}
+
+export default AdoptionListPage;
